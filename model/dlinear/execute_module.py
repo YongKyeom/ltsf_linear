07bacb5078897b1d2d4087de4b01b7250f61bef9
YongKyeom <kyeom4341@naver.com>
2024-11-25 00:11:03 +0900

    Apply initialize technique in DNLinear

--- model/dlinear/execute_module.py
+++ model/dlinear/execute_module.py
@@ -289,6 +289,35 @@ class DNLinearModel(nn.Module):
         else:
             self.Linear_Seasonal = nn.Linear(self.window_size, self.forecast_size)
             self.Linear_Trend = nn.Linear(self.window_size, self.forecast_size)
+    
+        # Weight initialization
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        """
+        Initialize weights for all layers
+        """
+        if isinstance(m, nn.Linear):
+            # Xavier Initialization for Linear layers
+            nn.init.xavier_uniform_(m.weight)
+            if m.bias is not None:
+                nn.init.zeros_(m.bias)
+        elif isinstance(m, nn.Conv1d):
+            # Kaiming (He) Initialization for Conv1d layers
+            nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            if m.bias is not None:
+                nn.init.zeros_(m.bias)
+        elif isinstance(m, nn.BatchNorm1d):
+            # BatchNorm Initialization: weight to 1, bias to 0
+            nn.init.ones_(m.weight)
+            nn.init.zeros_(m.bias)
+        elif isinstance(m, nn.MultiheadAttention):
+            # MultiheadAttention Initialization
+            for name, param in m.named_parameters():
+                if 'in_proj_weight' in name or 'out_proj.weight' in name:
+                    nn.init.xavier_uniform_(param)
+                elif 'in_proj_bias' in name or 'out_proj.bias' in name:
+                    nn.init.zeros_(param)
 
     def forward(self, x):
         """
