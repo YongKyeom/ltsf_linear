2d42df046d0f71d750f17e8667be423887c1579f
YongKyeom <kyeom4341@naver.com>
2024-11-28 20:54:47 +0900

    Add MSE in metric

--- common/metrics.py
+++ common/metrics.py
@@ -8,6 +8,12 @@ def compute_mae(y_true, y_pred):
     return mean_absolute_error(y_true_np, y_pred_np)
 
 
+def compute_mse(y_true, y_pred):
+    y_true_np = y_true.cpu().numpy().reshape(-1, y_true.shape[-1])
+    y_pred_np = y_pred.cpu().numpy().reshape(-1, y_pred.shape[-1])
+    return np.sqrt(mean_squared_error(y_true_np, y_pred_np))
+
+
 def compute_rmse(y_true, y_pred):
     y_true_np = y_true.cpu().numpy().reshape(-1, y_true.shape[-1])
     y_pred_np = y_pred.cpu().numpy().reshape(-1, y_pred.shape[-1])
